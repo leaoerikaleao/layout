628a4c9adb2ad428e48153f59ef7827b8810c8aa
Erika Leão <erika.lesantos@gmail.com>
2021-03-15 19:30:01 -0300

    ( fixind Index

--- pages/index.js
+++ pages/index.js
@@ -16,15 +16,14 @@ const Index = () => {
                 </h1>
 
                 <div className="container border-box bg-blue-800 text-white m-4 p-4 mx-auto border-2 border-blue-900">
-                    <p>Eu sou um Border-box! O meu tamanho total inclui a margin e o padding.<br /> Tamanho total = (tamanho do elemento) - padding - margin)</p>
+                    <p>Eu sou um Border-box! O meu tamanho total inclui a margin e o padding.<br /> Tamanho total = ((tamanho do elemento) - padding - margin)</p>
                 </div>
 
                 <div className="container box-content bg-green-700 text-white m-4 p-4 mx-auto border-2 border-green-800">
-                    <p>Eu sou um Box-content! O meu tamanho total é somado a margin e o padding. <br /> Tamanho total = (tamanho do elemento) + padding + margin)</p>
+                    <p>Eu sou um Box-content! O meu tamanho total é somado a margin e o padding. <br /> Tamanho total =((tamanho do elemento) + padding + margin)</p>
                 </div>
             </div>
         </div>
-
 
     )
 }
