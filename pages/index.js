f3c4a6c1603db577894788e2785da037fe82a8ac
Erika Leão <erika.lesantos@gmail.com>
2021-03-15 21:07:56 -0300

    Refactor Collors

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,23 @@ const Index = () => {
     return (
         <div>
             <div className="p-5">
-                <h1 className="text-3xl m-4 p-4 text-center mx-auto">Container</h1>
-                <div className="container bg-red-800 text-white m-4 p-4 mx-auto border-2 border-red-900">
+                <h1 className="text-3xl m-5 py-5 ">Container</h1>
+                <div className="container bg-red text-white p-5 mx-auto border-2 border-red-dark">
                     <p>Eu sou um Container! Possuo tamanho fixo e incluo outros elementos como este texto. </p>
                 </div>
             </div>
 
             <div className="p-5">
-                <h1 className="text-3xl m-4 p-4 text-center mx-auto">
+                <h1 className="text-3xl m-5 py-5 ">
                     Border-box e Box-content
                 </h1>
 
-                <div className="container border-box bg-blue-800 text-white m-4 p-4 mx-auto border-2 border-blue-900">
+                <div className="container border-box bg-blue text-white p-5 m-5  border-2 border-blue-dark ">
                     <p>Eu sou um Border-box! O meu tamanho total inclui a margin e o padding.<br /> Tamanho total = ((tamanho do elemento) - padding - margin)</p>
                 </div>
 
-                <div className="container box-content bg-green-700 text-white m-4 p-4 mx-auto border-2 border-green-800">
-                    <p>Eu sou um Box-content! O meu tamanho total é somado a margin e o padding. <br /> Tamanho total =((tamanho do elemento) + padding + margin)</p>
+                <div className="container box-content bg-green text-white p-5  border-2 border-green-dark ">
+                    <p>Eu sou um Box-content! O meu tamanho total é somado a margin e o padding. <br /> Tamanho total = ((tamanho do elemento) + padding + margin)</p>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      yellow: {
+        light: '#ddbd50',
+        DEFAULT: '#D5AD25',
+        dark: '#bf9b21'
+      },
+      green: {
+        light: '#6eba81',
+        DEFAULT: '#4AA962',
+        dark: '#429858'
+      },
+      blue: {
+        light: '#699cf7',
+        DEFAULT: '#4484f5',
+        dark: '#3d76dc',
+      },
+      white: {
+        DEFAULT: '#ffffff',
+      },
+      red: {
+        light: '#e6543f',
+        DEFAULT: '#E4422A',
+        dark: '#cd3b25'
+      },
+      gray: {
+        light: '#7D807D',
+        DEFAULT: '#505150',
+        dark: '#252625'
+      }
+    }
   },
   variants: {
     extend: {},
